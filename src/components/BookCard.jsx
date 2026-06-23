3740a5aa309e50570e805db7b2f29309a8a6a68f
Riki <rmcal024@uottawa.ca>
2026-06-23 10:07:30 -0400

    Fallback Placeholder Book Cover
    + Implemented a placeholder book cover image whenever book has no id

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,9 +1,15 @@
 function BookCard({book}) {
 
-    const cover_image = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+    let cover_image = ""
     const book_link = `https://openlibrary.org${book.key}`;
     const author_link = `https://openlibrary.org/authors/${book.author_key}`;
     const authors = book.author_name.join(", ");
+
+    if (book.cover_i) {
+        cover_image = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+    } else {
+        cover_image = "https://placehold.net/400x600.png";
+    }
 
     return(
         <>
