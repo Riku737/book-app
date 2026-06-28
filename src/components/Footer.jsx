c0ee74d9cd829ecee0cf7e42514196247d9c6bf4
Riki <rmcal024@uottawa.ca>
2026-06-27 20:41:07 -0400

    Footer placeholder content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,43 @@ function Footer() {
 
     return(
         <>
-        <div className="border-top">
-            <div className="container mt-3">
+        <div className="bg-light py-5">
+            <div className="container">
                 <div className="row">
 
-                    <div className="col-4">
-                        <h5>NavBar</h5>
+                    <div className="col-3">
+                        <h5>Bookstore</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed iaculis elit. Donec bibendum in elit in mollis.</p>
+                    </div>
+
+                    <div className="col-3">
+                        <h5>Shop</h5>
+                        <ul className="list-unstyled d-flex flex-column gap-2">
+                            <li>Text</li>
+                            <li>Text</li>
+                            <li>Text</li>
+                            <li>Text</li>
+                        </ul>
+                    </div>
+
+                    <div className="col-3">
+                        <h5>Help</h5>
+                        <ul className="list-unstyled d-flex flex-column gap-2">
+                            <li>Text</li>
+                            <li>Text</li>
+                            <li>Text</li>
+                            <li>Text</li>
+                        </ul>
+                    </div>
+
+                    <div className="col-2">
+                        <h5>About</h5>
+                        <ul className="list-unstyled d-flex flex-column gap-2">
+                            <li>Text</li>
+                            <li>Text</li>
+                            <li>Text</li>
+                            <li>Text</li>
+                        </ul>
                     </div>
 
                 </div>
